16f99b86aa127fed0daba5671dad83294f0a3054
oxo <>
2021-04-02 00:00:19 +0900

    add:入力欄「お名前」「メールアドレス」「お問い合わせ内容」を追加

--- Views/index.php
+++ Views/index.php
@@ -4,4 +4,12 @@
         echo '（・ｘ・）<br/>';
         echo 'お問い合わせフォーム';
     ?>
+    <form method="post">
+        お名前<br/>
+        <input type="text" name="name"><br/>
+        メールアドレス<br/>
+        <input type="text" name="mail"><br/>
+        お問い合わせ内容<br/>
+        <textarea name="inquiry"></textarea><br/>
+    </form>
 </html>
