b6301f8e5b4fbd8b091cff398e6a88283cc964e6
oxo <>
2021-04-04 19:30:27 +0900

    update:項目名をlabelタグに記述

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-<html>
 <html lang="ja">
     <?php
         echo '（・ｘ・）<br/>';
         echo 'お問い合わせフォーム';
     ?>
     <form action="Views/DoneInquiryView.php" method="post">
-        お名前<br/>
-        <input type="text" name="name"><br/>
-        メールアドレス<br/>
-        <input type="text" name="mail"><br/>
-        お問い合わせ内容<br/>
-        <textarea name="inquiry"></textarea><br/>
+        <label for="name">お名前<br/>
+            <input type="text" name="name">
+        </label>
+        <br/>
+        <label for="mail">メールアドレス<br/>
+            <input type="text" name="mail">
+        </label><br/>
+        <label for="inquiry">お問い合わせ内容<br/>
+            <textarea name="inquiry"></textarea>
+        </label><br/>
         <input type="submit" value="送信する">
     </form>
 </html>
